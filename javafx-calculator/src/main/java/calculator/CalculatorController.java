066a006a9ce339d5af790c1f4264dab44e83b864
raddanfea <43278151+raddanfea@users.noreply.github.com>
2020-05-17 15:26:23 +0200

    Add files via upload

---
+++ javafx-calculator/src/main/java/calculator/CalculatorController.java
@@ -0,0 +1,72 @@
+package calculator;
+
+import calculator.model.Calculator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CalculatorController {
+
+    @FXML
+    private TextField display;
+
+    private Calculator calculator;
+    private boolean startNumber = true;
+    private double number1;
+    private String operator = "";
+
+    @FXML
+    private void initialize() {
+        calculator = new Calculator();
+        display.setText("0");
+        operator = "";
+    }
+
+
+    @FXML
+    public void processDigit(ActionEvent event) {
+        String digitPressed = ((Button) event.getSource()).getText();
+        System.out.println(digitPressed);
+        if (startNumber || display.getText().equals("0")) {
+            display.setText(digitPressed);
+        } else {
+            display.setText(display.getText() + digitPressed);
+        }
+        startNumber = false;
+    }
+
+    @FXML
+    public void processOperator(ActionEvent event) {
+        String operatorPressed = ((Button) event.getSource()).getText();
+        System.out.println(operatorPressed);
+
+        if (operatorPressed.equals(".")){
+            Character lastchar = display.getText().charAt(display.getText().length() - 1);
+            if (!lastchar.equals('.')){
+                display.setText(display.getText() + ".");
+            }
+        }
+        else if (operatorPressed.equals("±")){
+            display.setText(String.format("%.0f",Double.parseDouble(display.getText()) * -1));
+        }
+        else if (operatorPressed.equals("=")) {
+           if (operator.isEmpty()) {
+               return;
+           }
+           double number2 = Double.parseDouble(display.getText());
+           double result = calculator.calculate(number1, number2, operator);
+           display.setText(String.format("%.0f", result));
+           operator = "";
+        }
+        else{
+            if (! operator.isEmpty()) {
+                return;
+            }
+            number1 = Double.parseDouble(display.getText());
+            operator = operatorPressed;
+            startNumber = true;
+        }
+    }
+
+}
