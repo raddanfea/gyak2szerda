ec7fddfa6a6e98c6a17994c49102a44b9bcb6c8d
raddanfea <radidani55@gmail.com>
2020-05-25 01:39:53 +0200

    tests

---
+++ rpgsheet/src/test/java/characters/CharacterBaseTest.java
@@ -0,0 +1,54 @@
+package characters;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterBaseTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRpgclass() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAbilities() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSkillsList() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItems() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStats() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLevel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRace() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGender() {
+    }
+}
